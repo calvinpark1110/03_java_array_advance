c3173d6e84366a247eba52d9dbb878362f7968ef
Calvin <75257676+clavinpark1110@users.noreply.github.com>
2021-06-06 22:43:44 +0900

    firstCommit

---
+++ 03_java_array_advance/src/step3_01/arrayAdvance/Tester.java
@@ -0,0 +1,13 @@
+package step3_01.arrayAdvance;
+
+import java.util.Scanner;
+
+public class Tester {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+	
+	}
+
+}
